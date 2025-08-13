4aad59315214f3e499ecfd6dc10f6b0638b871c1
Pavel Davidouski <paul.davidovski@gmail.com>
2025-08-13 09:44:08 +0300

    Adds SFTP configuration files
    Adds configuration files for SFTP to allow automated deployments.
    
    Renames the `configs.yml` file for Homer to improve project structure and consistency.
    
    Updates the Wi-Fi manager script to prevent it from unnecessarily blocking the Wi-Fi connection when it's already disabled.

--- scripts/wifi_lan_managers/tools/wifi_lan_manager.py
+++ scripts/wifi_lan_managers/tools/wifi_lan_manager.py
@@ -9,7 +9,6 @@ from common.logger import setup_logger
 
 log = setup_logger('LanWifiPolicy', '/home/reekroo/scripts/logs/wifi_manager.log')
 
-
 class LanWifiPolicy:
     def __init__(self, lan_interface_name='eth0'):
         self.lan_monitor = InterfaceMonitor(lan_interface_name)
@@ -17,7 +16,7 @@ class LanWifiPolicy:
         log.info("[LanWifiPolicy] LAN/Wi-Fi Policy Manager Initialized.")
 
     def apply(self):
-        if not self.lan_monitor.is_up():            
+        if not self.lan_monitor.is_up():
             log.info("[LanWifiPolicy] LAN connection not detected. No action needed.")
             return
 
@@ -26,7 +25,7 @@ class LanWifiPolicy:
         if self.wifi_controller.is_blocked():
             log.info("[LanWifiPolicy] Wi-Fi is already disabled. No action needed.")
             return
-        
+
         log.info("[LanWifiPolicy] Wi-Fi is active, applying block...")
         self.wifi_controller.set_blocked(True)
 
